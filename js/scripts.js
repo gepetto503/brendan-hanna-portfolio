95d7bab8594c35d5562e89c33a81bb766dcacea4
Brendan Hanna <gepetto503@gmail.com>
2017-12-07 20:37:15 -0800

    tried to get materialize navbar working but didnt.  now forking to try my own navbar

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,25 @@
 $( document ).ready(function() {
-    console.log( "ready!" );
+    $(".button-collapse").sideNav();
+    $(window).scroll(function(){
+    	if($(window).scrollTop() >= 300) {
+    		$('nav').addClass('sticky');
+    	} else {
+    		$('nav').removeClass('sticky');
+    	}
+    });
+
+    $('nav a').click(function(event) {
+
+    	var id = $(this).attr("href");
+    	var offset = 100;
+    	var target = $(id).offset().top - offset;
+    	$('html, body').animate({
+    		scrollTop: target });
+    });
+
+    $('#hide-nav').click(function(event){
+      console.log("hi");
+      $('.button-collapse').sideNav('hide');
+    });
+
 });
