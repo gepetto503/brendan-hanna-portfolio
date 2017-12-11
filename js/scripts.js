7453a41c0b2af3cb3d2144793323e9cd503c6ada
Brendan Hanna <gepetto503@gmail.com>
2017-12-10 23:45:06 -0800

    successfully made first carousel slide non hover on touch devices

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,11 @@
 $( document ).ready(function() {
-  window.USER_IS_TOUCHING = null;
-
-  //when user touches screen...
-  window.addEventListener('touchstart', function onFirstTouch() {
-    // or set some global variable
-    window.USER_IS_TOUCHING = true;
-    alert('wee');
-
-    // we only need to know once that a human touched the screen, so we can stop listening now
-    window.removeEventListener('touchstart', onFirstTouch, false);
-  }, false);
 
   //make room for navbar
   $('body').offset({top:100, left: 0});
-
+  //make dropdown open and close when hamburger
+  $('.hamburger').click(function(event){
+    $('.dropdown-links').toggle();
+  });
   //make dropdown close when you click off it
   $('html').click(function() {
     $('.dropdown-links').hide();
@@ -22,10 +14,6 @@ $( document ).ready(function() {
     event.stopPropagation();
   });
 
-  //make dropdown close when you click hamburger
-  $('.hamburger').click(function(event){
-    $('.dropdown-links').toggle();
-  });
 
   //make page scroll to section when you click nav link
   $('.my-nav a, #nav-logo').click(function(event) {
@@ -40,50 +28,12 @@ $( document ).ready(function() {
 
   //initialize materialize carousel
  $('.carousel.carousel-slider').carousel({fullWidth: true});
-
- //make my previous and next chevrons on carousel Work
+ //make my previous and next arrows on carousel Work
  $('.carousel-prev').click(function(event){
    $('.carousel').carousel('prev');
  });
  $('.carousel-next').click(function(event){
    $('.carousel').carousel('next');
  });
-
- //on page load, individual case studies are hidden until their corresponding view button is clicked
- $('.case-study').hide();
-
- //when you click a button on carousel, slide in case study
- $('.view-proj-btn').click(function(event){
-   //slide in case studies div from the right
-   $('.swapper').addClass('case-studies-visible');
-   //dissappear about and contact sections
-   $('header, #about, #contact').toggle([400]);
- });
-
- //making button show corresponding case study
-  $('#button-1').click(function(event){
-    $('.case-study').hide();
-    $('#case-study-1').show();
-  });
-   $('#button-2').click(function(event){
-     $('.case-study').hide();
-     $('#case-study-2').show();
-   });
-   $('#button-3').click(function(event){
-     $('.case-study').hide();
-     $('#case-study-3').show();
-   });
-   $('#button-4').click(function(event){
-     $('.case-study').hide();
-     $('#case-study-4').show();
-   });
-
- //when you click the back arrow on case study, bring back carousel
- $('.back-chevron, .back').click(function(event){
-   $('.swapper').removeClass('case-studies-visible');
-   $('header, #about, #contact').toggle();
-  });
-
-  console.log(window.USER_IS_TOUCHING);
 
 });
